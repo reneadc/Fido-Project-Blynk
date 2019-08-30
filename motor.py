078b9c0e4027c857126503dcc5d545b8f134488c
reneadc <reneadc96@gmail.com>
2019-08-29 17:35:58 -0700

    Updated Motor Script

--- motor.py
+++ motor.py
@@ -5,6 +5,7 @@ GPIO.setup(07, GPIO.OUT)
 pwm=GPIO.PWM(07, 50)
 pwm.start(0)
 
+#ServoMotor Code
 def SetAngle(angle):
 	duty = angle / 18 + 2
 	GPIO.output(07, True)
@@ -13,31 +14,36 @@ def SetAngle(angle):
 	GPIO.output(07, False)
 	pwm.ChangeDutyCycle(0)
 
-if __name__== "__main__":
+def runMotor(rotations):
 	counter = 1
 	down = False
 	angle = 0
-	while(counter <= 6):
-		#if(angle <= 180 ):
-			#if(angle == 180):
-			#	down = true
-			#else if(angle < 180 and down = false):
-			#	angle -= 60
-			#else:
-			#	angle += 60
-		
-		#else if(angle == 180 and down == true):
-		#	angle-= 60	
-		if(angle == 180):
+	while (counter <= rotations):
+		# if(angle <= 180 ):
+		# if(angle == 180):
+		#	down = true
+		# else if(angle < 180 and down = false):
+		#	angle -= 60
+		# else:
+		#	angle += 60
+
+		# else if(angle == 180 and down == true):
+		#	angle-= 60
+		if (angle == 180):
 			down = True
-		elif(angle == 0):
+		elif (angle == 0):
 			down = False
-		if(down == False):
+		if (down == False):
 			angle += 60
 		else:
-			angle -= 60		 
+			angle -= 60
 		SetAngle(angle)
-		counter +=1
+		counter += 1
+
+
+if __name__== "__main__":
+
+	runMotor(6)
 	pwm.stop()
 	GPIO.cleanup()
 	
